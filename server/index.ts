94d5d87ea72cb218b9cd6a898c8d006c81350912
Your Name <himanshumeena2572006@gmail.com>
2025-07-29 05:29:04 -0700

    push

--- server/index.ts
+++ server/index.ts
@@ -73,7 +73,7 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  const port = parseInt(process.env.PORT || '5000', 10);
+  const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 5000;
   server.listen({
     port,
     host: "0.0.0.0"
